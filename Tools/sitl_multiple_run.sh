e3d63fb0709c1f6d9b840d263e4d9662e6b4755c
korotkoves <korotkov.es@mipt.ru>
2016-07-02 23:24:56 +0300

    multi sitl for iris_opt_flow

--- Tools/sitl_multiple_run.sh
+++ Tools/sitl_multiple_run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 sitl_num=$1
+model=$2
 
 sim_port=15019
 mav_port=15010
 mav_port2=15011
+mav_port3=15012
 
 mav_oport=15015
 mav_oport2=15016
+mav_oport3=15017
 
 port_step=10
 
@@ -18,24 +21,30 @@ build_path=${src_path}/build_posix_sitl_default
 
 cd $build_path/src/firmware/posix
 
+rc_file="rcS"
 n=1
 while [ $n -le $sitl_num ]; do
- if [ ! -d $n ]; then
-  mkdir -p $n
-  cd $n
+ wrk_dir=$model$n
 
+ mkdir -p $wrk_dir
+ cd $wrk_dir
+
+ if [ ! -f $rc_file ]; then
   mkdir -p rootfs/fs/microsd
   mkdir -p rootfs/eeprom
   touch rootfs/eeprom/parameters
 
   cp ${src_path}/ROMFS/px4fmu_common/mixers/quad_w.main.mix ./
-  cat ${src_path}/${rc_script}_gazebo_iris | sed s/_SIMPORT_/${sim_port}/ | sed s/_MAVPORT_/${mav_port}/g | sed s/_MAVOPORT_/${mav_oport}/ | sed s/_MAVPORT2_/${mav_port2}/ | sed s/_MAVOPORT2_/${mav_oport2}/ | sed s/_SYSID_/$n/ > rcS
-  cd ../
+
+  cat ${src_path}/${rc_script}_gazebo_$model |
+    sed s/_SYSID_/$n/ |
+    sed s/_SIMPORT_/${sim_port}/ |
+    sed s/_MAVPORT_/${mav_port}/g | sed s/_MAVOPORT_/${mav_oport}/ |
+    sed s/_MAVPORT2_/${mav_port2}/ | sed s/_MAVOPORT2_/${mav_oport2}/ |
+    sed s/_MAVPORT3_/${mav_port3}/g | sed s/_MAVOPORT3_/${mav_oport3}/ > $rc_file
  fi
 
- cd $n
-
- sudo -b -u $USER ../mainapp -d rcS >out.log 2>err.log
+ sudo -b -u $USER ../mainapp -d $rc_file >out.log 2>err.log
 
  cd ../
 
@@ -43,6 +52,8 @@ while [ $n -le $sitl_num ]; do
  sim_port=$(($sim_port + $port_step))
  mav_port=$(($mav_port + $port_step))
  mav_port2=$(($mav_port2 + $port_step))
+ mav_port3=$(($mav_port3 + $port_step))
  mav_oport=$(($mav_oport + $port_step))
  mav_oport2=$(($mav_oport2 + $port_step))
+ mav_oport3=$(($mav_oport3 + $port_step))
 done
